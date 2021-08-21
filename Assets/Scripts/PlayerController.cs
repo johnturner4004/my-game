a14d01d09acb02e5b1534750f2bf29b530d0007f
johnturner4004 <johnturner4004@gmail.com>
2021-08-20 23:27:10 -0400

    :tada: add horizontal input for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   public float jumpForce;
+  public float moveSpeed;
   private Rigidbody playerRigidbody;
   public bool isOnGround = true;
+  private float horizontalInput;
   // Start is called before the first frame update
   void Start()
   {
@@ -21,6 +23,9 @@ public class PlayerController : MonoBehaviour
       playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
       isOnGround = false;
     }
+
+    horizontalInput = Input.GetAxis("Horizontal");
+    transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * horizontalInput);
   }
 
   private void OnCollisionEnter(Collision collision)
